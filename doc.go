6a327c0bcbd6cbfeec3734c2497dac6fbfc4fcc3
henrylee2cn <henrylee_cn@foxmail.com>
2017-08-25 02:18:07 +0800

    Update README and comment

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 // Teleport is a versatile, high-performance and flexible TCP socket framework.
 // It can be used for RPC, micro services, peer-peer, push services, game services and so on.
-
+//
 // Copyright 2015-2017 HenryLee. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
