f05921ddcaa22b7f697d0e3b88dc14cdb2d2488e
henrylee2cn <henrylee_cn@foxmail.com>
2017-08-26 03:05:20 +0800

    optimized codes

--- codec/json_codec.go
+++ codec/json_codec.go
@@ -19,11 +19,14 @@ import (
 	"encoding/json"
 	"io"
 
+	"github.com/json-iterator/go"
+
 	"github.com/henrylee2cn/teleport/utils"
 )
 
 func init() {
 	// Reg(new(JsonCodec))
+	Reg(new(JsoniterCodec))
 }
 
 // JsonCodec json codec
@@ -83,4 +86,63 @@ func (p *JsonDecoder) Decode(v interface{}) error {
 		return err
 	}
 	return json.Unmarshal(p.buf.Bytes(), v)
+}
+
+// JsoniterCodec json codec
+type JsoniterCodec struct{}
+
+// Name returns codec name
+func (j *JsoniterCodec) Name() string {
+	return "json"
+}
+
+//
+// Id returns codec id
+func (j *JsoniterCodec) Id() byte {
+	return 'j'
+}
+
+// JsoniterEncoder json decoder
+type JsoniterEncoder struct {
+	writer io.Writer
+}
+
+// NewEncoder returns a new json encoder that writes to writer.
+func (*JsoniterCodec) NewEncoder(writer io.Writer) Encoder {
+	return &JsoniterEncoder{writer}
+}
+
+// Encode writes the json encoding of v to the writer.
+func (p *JsoniterEncoder) Encode(v interface{}) error {
+	b, err := jsoniter.Marshal(v)
+	if err != nil {
+		return err
+	}
+	_, err = p.writer.Write(b)
+	return err
+}
+
+// JsoniterDecoder json decoder
+type JsoniterDecoder struct {
+	limitReader io.Reader
+	buf         *bytes.Buffer
+}
+
+// NewDecoder returns a new json decoder that reads from limit reader.
+func (*JsoniterCodec) NewDecoder(limitReader io.Reader) Decoder {
+	return &JsoniterDecoder{
+		limitReader: limitReader,
+		buf:         bytes.NewBuffer(make([]byte, 0, bytes.MinRead)),
+	}
+}
+
+// Decode reads the next json-encoded value from its
+// input and stores it in the value pointed to by v.
+func (p *JsoniterDecoder) Decode(v interface{}) error {
+	p.buf.Reset()
+	err := utils.ReadAll(p.limitReader, p.buf)
+	if err != nil && err != io.ErrUnexpectedEOF {
+		return err
+	}
+	return jsoniter.Unmarshal(p.buf.Bytes(), v)
 }
