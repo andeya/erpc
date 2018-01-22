f38004bbd0feab45ee4e9343261eefb29c171ab6
henrylee2cn <henrylee_cn@foxmail.com>
2018-01-22 18:44:41 +0800

    Optimize codes

--- socket/socket.go
+++ socket/socket.go
@@ -35,10 +35,8 @@ type (
 	Socket interface {
 		// LocalAddr returns the local network address.
 		LocalAddr() net.Addr
-
 		// RemoteAddr returns the remote network address.
 		RemoteAddr() net.Addr
-
 		// SetDeadline sets the read and write deadlines associated
 		// with the connection. It is equivalent to calling both
 		// SetReadDeadline and SetWriteDeadline.
@@ -55,44 +53,33 @@ type (
 		//
 		// A zero value for t means I/O operations will not time out.
 		SetDeadline(t time.Time) error
-
 		// SetReadDeadline sets the deadline for future Read calls
 		// and any currently-blocked Read call.
 		// A zero value for t means Read will not time out.
 		SetReadDeadline(t time.Time) error
-
 		// SetWriteDeadline sets the deadline for future Write calls
 		// and any currently-blocked Write call.
 		// Even if write times out, it may return n > 0, indicating that
 		// some of the data was successfully written.
 		// A zero value for t means Write will not time out.
 		SetWriteDeadline(t time.Time) error
-
 		// WritePacket writes header and body to the connection.
 		// Note: must be safe for concurrent use by multiple goroutines.
 		WritePacket(packet *Packet) error
-
 		// ReadPacket reads header and body from the connection.
 		// Note: must be safe for concurrent use by multiple goroutines.
 		ReadPacket(packet *Packet) error
-
 		// Read reads data from the connection.
 		// Read can be made to time out and return an Error with Timeout() == true
 		// after a fixed time limit; see SetDeadline and SetReadDeadline.
 		Read(b []byte) (n int, err error)
-
 		// Write writes data to the connection.
 		// Write can be made to time out and return an Error with Timeout() == true
 		// after a fixed time limit; see SetDeadline and SetWriteDeadline.
 		Write(b []byte) (n int, err error)
-
-		// Reset reset net.Conn
-		// Reset(net.Conn)
-
 		// Close closes the connection socket.
 		// Any blocked Read or Write operations will be unblocked and return errors.
 		Close() error
-
 		// Public returns temporary public data of Socket.
 		Public() goutil.Map
 		// PublicLen returns the length of public data of Socket.
