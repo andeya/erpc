3553ebacef22cf3d0a1a42cdbfa358bb0e675787
henrylee2cn <henrylee_cn@foxmail.com>
2018-01-23 17:02:48 +0800

    Specify the type of authorization info

--- samples/auth/client.go
+++ samples/auth/client.go
@@ -8,13 +8,13 @@ import (
 func main() {
 	cli := tp.NewPeer(tp.PeerConfig{}, plugin.LaunchAuth(generateAuthInfo))
 	defer cli.Close()
-	_, e := cli.Dial(":9090")
-	if e != nil {
-		tp.Fatalf("%v", e)
+	_, rerr := cli.Dial(":9090")
+	if rerr != nil {
+		tp.Fatalf("%v", rerr)
 	}
 }
 
-const clientAuthInfo = "client auth info"
+const clientAuthInfo = "client-auth-info-12345"
 
 func generateAuthInfo() string {
 	tp.Infof("generate auth info: %s", clientAuthInfo)
