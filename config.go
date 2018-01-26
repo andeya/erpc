9f6796e793ba2d3daa0615fe46c485d280647cc2
henrylee2cn <henrylee_cn@foxmail.com>
2018-01-26 11:11:33 +0800

    adjust the code location

--- config.go
+++ config.go
@@ -20,6 +20,7 @@ import (
 	"time"
 
 	"github.com/henrylee2cn/cfgo"
+	"github.com/henrylee2cn/teleport/socket"
 )
 
 // PeerConfig peer config
@@ -69,3 +70,62 @@ func (p *PeerConfig) check() error {
 	}
 	return nil
 }
+
+// DefaultProtoFunc gets the default builder of socket communication protocol
+//  func DefaultProtoFunc() socket.ProtoFunc
+var DefaultProtoFunc = socket.DefaultProtoFunc
+
+// SetDefaultProtoFunc sets the default builder of socket communication protocol
+//  func SetDefaultProtoFunc(protoFunc socket.ProtoFunc)
+var SetDefaultProtoFunc = socket.SetDefaultProtoFunc
+
+// GetReadLimit gets the packet size upper limit of reading.
+//  PacketSizeLimit() uint32
+var GetReadLimit = socket.PacketSizeLimit
+
+// SetReadLimit sets max packet size.
+// If maxSize<=0, set it to max uint32.
+//  func SetPacketSizeLimit(maxPacketSize uint32)
+var SetReadLimit = socket.SetPacketSizeLimit
+
+// SetSocketKeepAlive sets whether the operating system should send
+// keepalive messages on the connection.
+// Note: If have not called the function, the system defaults are used.
+//  func SetSocketKeepAlive(keepalive bool)
+var SetSocketKeepAlive = socket.SetKeepAlive
+
+// SetSocketKeepAlivePeriod sets period between keep alives.
+// Note: if d<0, don't change the value.
+//  func SetSocketKeepAlivePeriod(d time.Duration)
+var SetSocketKeepAlivePeriod = socket.SetKeepAlivePeriod
+
+// SocketReadBuffer returns the size of the operating system's
+// receive buffer associated with the connection.
+// Note: if using the system default value, bytes=-1 and isDefault=true.
+//  func ReadBuffer() (bytes int, isDefault bool)
+var SocketReadBuffer = socket.ReadBuffer
+
+// SetSocketReadBuffer sets the size of the operating system's
+// receive buffer associated with the connection.
+// Note: if bytes<0, don't change the value.
+//  func SetReadBuffer(bytes int)
+var SetSocketReadBuffer = socket.SetReadBuffer
+
+// SocketWriteBuffer returns the size of the operating system's
+// transmit buffer associated with the connection.
+// Note: if using the system default value, bytes=-1 and isDefault=true.
+//  func WriteBuffer() (bytes int, isDefault bool)
+var SocketWriteBuffer = socket.WriteBuffer
+
+// SetSocketWriteBuffer sets the size of the operating system's
+// transmit buffer associated with the connection.
+// Note: if bytes<0, don't change the value.
+//  func SetWriteBuffer(bytes int)
+var SetSocketWriteBuffer = socket.SetWriteBuffer
+
+// SetSocketNoDelay controls whether the operating system should delay
+// packet transmission in hopes of sending fewer packets (Nagle's
+// algorithm).  The default is true (no delay), meaning that data is
+// sent as soon as possible after a Write.
+//  func SetNoDelay(noDelay bool)
+var SetSocketNoDelay = socket.SetNoDelay
--- utils.go
+++ utils.go
@@ -35,65 +35,6 @@ var GetPacket = socket.GetPacket
 //  func PutPacket(p *socket.Packet)
 var PutPacket = socket.PutPacket
 
-// DefaultProtoFunc gets the default builder of socket communication protocol
-//  func DefaultProtoFunc() socket.ProtoFunc
-var DefaultProtoFunc = socket.DefaultProtoFunc
-
-// SetDefaultProtoFunc sets the default builder of socket communication protocol
-//  func SetDefaultProtoFunc(protoFunc socket.ProtoFunc)
-var SetDefaultProtoFunc = socket.SetDefaultProtoFunc
-
-// GetReadLimit gets the packet size upper limit of reading.
-//  PacketSizeLimit() uint32
-var GetReadLimit = socket.PacketSizeLimit
-
-// SetReadLimit sets max packet size.
-// If maxSize<=0, set it to max uint32.
-//  func SetPacketSizeLimit(maxPacketSize uint32)
-var SetReadLimit = socket.SetPacketSizeLimit
-
-// SetSocketKeepAlive sets whether the operating system should send
-// keepalive messages on the connection.
-// Note: If have not called the function, the system defaults are used.
-//  func SetSocketKeepAlive(keepalive bool)
-var SetSocketKeepAlive = socket.SetKeepAlive
-
-// SetSocketKeepAlivePeriod sets period between keep alives.
-// Note: if d<0, don't change the value.
-//  func SetSocketKeepAlivePeriod(d time.Duration)
-var SetSocketKeepAlivePeriod = socket.SetKeepAlivePeriod
-
-// SocketReadBuffer returns the size of the operating system's
-// receive buffer associated with the connection.
-// Note: if using the system default value, bytes=-1 and isDefault=true.
-//  func ReadBuffer() (bytes int, isDefault bool)
-var SocketReadBuffer = socket.ReadBuffer
-
-// SetSocketReadBuffer sets the size of the operating system's
-// receive buffer associated with the connection.
-// Note: if bytes<0, don't change the value.
-//  func SetReadBuffer(bytes int)
-var SetSocketReadBuffer = socket.SetReadBuffer
-
-// SocketWriteBuffer returns the size of the operating system's
-// transmit buffer associated with the connection.
-// Note: if using the system default value, bytes=-1 and isDefault=true.
-//  func WriteBuffer() (bytes int, isDefault bool)
-var SocketWriteBuffer = socket.WriteBuffer
-
-// SetSocketWriteBuffer sets the size of the operating system's
-// transmit buffer associated with the connection.
-// Note: if bytes<0, don't change the value.
-//  func SetWriteBuffer(bytes int)
-var SetSocketWriteBuffer = socket.SetWriteBuffer
-
-// SetSocketNoDelay controls whether the operating system should delay
-// packet transmission in hopes of sending fewer packets (Nagle's
-// algorithm).  The default is true (no delay), meaning that data is
-// sent as soon as possible after a Write.
-//  func SetNoDelay(noDelay bool)
-var SetSocketNoDelay = socket.SetNoDelay
-
 var (
 	_maxGoroutinesAmount      = (1024 * 1024 * 8) / 8 // max memory 8GB (8KB/goroutine)
 	_maxGoroutineIdleDuration time.Duration
