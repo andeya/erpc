bdc6e77d79a89852a0c11fc7e5d4ca23d170ced7
henrylee2cn <henrylee_cn@foxmail.com>
2018-04-04 15:13:45 +0800

    codec: update plain codec

--- codec/plain_codec.go
+++ codec/plain_codec.go
@@ -16,6 +16,8 @@ package codec
 
 import (
 	"fmt"
+	"reflect"
+	"strconv"
 
 	"github.com/henrylee2cn/goutil"
 )
@@ -57,9 +59,43 @@ func (PlainCodec) Marshal(v interface{}) ([]byte, error) {
 	case *[]byte:
 		b = *s
 	default:
-		return nil, fmt.Errorf("string codec: %T can not be directly converted to []byte type", v)
+		var ok bool
+		b, ok = formatProperType(reflect.ValueOf(v))
+		if !ok {
+			return nil, fmt.Errorf("plain codec: %T can not be directly converted to []byte type", v)
+		}
 	}
 	return b, nil
+}
+
+func formatProperType(v reflect.Value) ([]byte, bool) {
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	switch v.Kind() {
+	case reflect.String:
+		return goutil.StringToBytes(v.String()), true
+	case reflect.Bool:
+		return goutil.StringToBytes(strconv.FormatBool(v.Bool())), true
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return goutil.StringToBytes(strconv.FormatInt(v.Int(), 10)), true
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return goutil.StringToBytes(strconv.FormatUint(v.Uint(), 10)), true
+	case reflect.Float32:
+		return goutil.StringToBytes(strconv.FormatFloat(v.Float(), 'f', -1, 32)), true
+	case reflect.Float64:
+		return goutil.StringToBytes(strconv.FormatFloat(v.Float(), 'f', -1, 64)), true
+	case reflect.Slice:
+		if v.Type().Elem().Kind() == reflect.Uint8 {
+			return v.Bytes(), true
+		} else {
+			return nil, false
+		}
+	case reflect.Invalid:
+		return []byte{}, true
+	default:
+		return nil, false
+	}
 }
 
 // Unmarshal parses the string-encoded data and stores the result
@@ -76,7 +112,57 @@ func (PlainCodec) Unmarshal(data []byte, v interface{}) error {
 		*s = make([]byte, len(data))
 		copy(*s, data)
 	default:
-		return fmt.Errorf("string codec: []byte can not be directly converted to %T type", v)
+		if !parseProperType(data, reflect.ValueOf(v)) {
+			return fmt.Errorf("plain codec: []byte can not be directly converted to %T type", v)
+		}
 	}
 	return nil
+}
+
+func parseProperType(data []byte, v reflect.Value) bool {
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	if !v.CanSet() {
+		return false
+	}
+	s := goutil.BytesToString(data)
+	switch v.Kind() {
+	case reflect.String:
+		v.SetString(s)
+	case reflect.Bool:
+		bol, err := strconv.ParseBool(s)
+		if err != nil {
+			return false
+		}
+		v.SetBool(bol)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		d, err := strconv.ParseInt(s, 10, 64)
+		if err != nil {
+			return false
+		}
+		v.SetInt(d)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		d, err := strconv.ParseUint(s, 10, 64)
+		if err != nil {
+			return false
+		}
+		v.SetUint(d)
+	case reflect.Float32, reflect.Float64:
+		f, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			return false
+		}
+		v.SetFloat(f)
+	case reflect.Slice:
+		if v.Type().Elem().Kind() != reflect.Uint8 {
+			return false
+		}
+		v.SetBytes(data)
+	case reflect.Invalid:
+		return true
+	default:
+		return false
+	}
+	return true
 }
