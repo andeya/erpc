5fe7c62479e3823c63ad5f237bca532c0ddd4f5c
henrylee2cn <henrylee_cn@foxmail.com>
2018-04-04 13:23:29 +0800

    codec: optimize comment

--- codec/_jsoniter_codec.go
+++ codec/_jsoniter_codec.go
@@ -23,6 +23,12 @@ import (
 	"github.com/henrylee2cn/teleport/utils"
 )
 
+// json codec name and id
+const (
+	NAME_JSON = "json"
+	ID_JSON   = 'j'
+)
+
 func init() {
 	Reg(new(JsoniterCodec))
 }
@@ -30,13 +36,12 @@ func init() {
 // JsoniterCodec json codec
 type JsoniterCodec struct{}
 
-// Name returns codec name
+// Name returns codec name.
 func (j *JsoniterCodec) Name() string {
 	return "json"
 }
 
-//
-// Id returns codec id
+// Id returns codec id.
 func (j *JsoniterCodec) Id() byte {
 	return 'j'
 }
