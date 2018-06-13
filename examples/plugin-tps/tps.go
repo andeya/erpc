80dcf87100a9197fe02b3b624f3e39f512d7e65d
henrylee2cn <henrylee_cn@foxmail.com>
2018-06-13 10:06:56 +0800

    examples: add tps plugin example

---
+++ examples/plugin-tps/tps.go
@@ -0,0 +1,67 @@
+// Package tps statistics requests per second
+package tps
+
+import (
+	"log"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	tp "github.com/henrylee2cn/teleport"
+)
+
+func NewTPS(intervalSecond uint32) *TPS {
+	if intervalSecond < 1 {
+		intervalSecond = 1
+	}
+	return &TPS{
+		stat:           map[string]*uint32{},
+		intervalSecond: intervalSecond,
+	}
+}
+
+type TPS struct {
+	stat           map[string]*uint32
+	intervalSecond uint32
+	once           sync.Once
+}
+
+var (
+	_ tp.PostRegPlugin          = (*TPS)(nil)
+	_ tp.PostWriteReplyPlugin   = (*TPS)(nil)
+	_ tp.PostReadPushBodyPlugin = (*TPS)(nil)
+)
+
+func (t *TPS) start() {
+	go func() {
+		ticker := time.NewTicker(time.Second * time.Duration(t.intervalSecond))
+		intervalSecond := float32(t.intervalSecond)
+		for {
+			<-ticker.C
+			for k, v := range t.stat {
+				log.Printf("[TPS] %s: %f in last %ds", k, float32(atomic.SwapUint32(v, 0))/intervalSecond, t.intervalSecond)
+			}
+		}
+	}()
+}
+
+func (t *TPS) Name() string {
+	return "TPS"
+}
+
+func (t *TPS) PostReg(h *tp.Handler) error {
+	t.stat[h.Name()] = new(uint32)
+	return nil
+}
+
+func (t *TPS) PostWriteReply(ctx tp.WriteCtx) *tp.Rerror {
+	t.once.Do(t.start)
+	atomic.AddUint32(t.stat[ctx.Output().UriObject().Path], 1)
+	return nil
+}
+
+func (t *TPS) PostReadPushBody(ctx tp.ReadCtx) *tp.Rerror {
+	t.once.Do(t.start)
+	atomic.AddUint32(t.stat[ctx.Path()], 1)
+	return nil
+}
---
+++ examples/plugin-tps/tps_test.go
@@ -0,0 +1,58 @@
+package tps
+
+import (
+	"testing"
+	"time"
+
+	tp "github.com/henrylee2cn/teleport"
+)
+
+type Pull struct {
+	tp.PullCtx
+}
+
+func (*Pull) Test(*struct{}) (*struct{}, *tp.Rerror) {
+	return nil, nil
+}
+
+type Push struct {
+	tp.PushCtx
+}
+
+func (*Push) Test(*struct{}) *tp.Rerror {
+	return nil
+}
+
+func TestTPS(t *testing.T) {
+	tp.SetLoggerLevel("OFF")
+	// Server
+	srv := tp.NewPeer(tp.PeerConfig{ListenAddress: ":9090"}, NewTPS(5))
+	srv.RoutePull(new(Pull))
+	srv.RoutePush(new(Push))
+	go srv.ListenAndServe()
+	time.Sleep(1e9)
+
+	// Client
+	cli := tp.NewPeer(tp.PeerConfig{})
+	sess, err := cli.Dial(":9090")
+	if err != nil {
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	var (
+		rerr   *tp.Rerror
+		ticker = time.NewTicker(time.Millisecond * 10)
+	)
+	for i := 0; i < 1<<10; i++ {
+		<-ticker.C
+		rerr = sess.Pull("/pull/test", nil, nil).Rerror()
+		if rerr != nil {
+			t.Error(rerr)
+		}
+		rerr = sess.Push("/push/test", nil)
+		if rerr != nil {
+			t.Error(rerr)
+		}
+	}
+}
