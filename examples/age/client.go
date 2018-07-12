239ac85d4a9ebea47f2b4990a3625780fd814ccf
henrylee2cn <henrylee_cn@foxmail.com>
2018-07-12 17:45:24 +0800

    session: add CloseNotify method to notify the connection that it  gone away.

--- examples/age/client.go
+++ examples/age/client.go
@@ -10,7 +10,6 @@ import (
 func main() {
 	tp.SetLoggerLevel("INFO")
 	cli := tp.NewPeer(tp.PeerConfig{PrintDetail: true})
-	defer cli.Close()
 	sess, err := cli.Dial(":9090")
 	if err != nil {
 		tp.Fatalf("%v", err)
@@ -18,13 +17,14 @@ func main() {
 
 	var result string
 	sess.Pull("/test/ok", "test1", &result)
-	tp.Infof("test sync 1: %v", result)
-
-	sess.Pull("/test/timeout", "test2", &result).Rerror()
-	tp.Infof("test sync 2: server context timeout: %v", result)
+	tp.Infof("test sync1: %v", result)
+	result = ""
+	rerr := sess.Pull("/test/timeout", "test2", &result).Rerror()
+	tp.Infof("test sync2: server context timeout: %v", rerr)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 
+	result = ""
 	pullCmd := sess.AsyncPull(
 		"/test/timeout",
 		"test3",
@@ -35,12 +35,24 @@ func main() {
 	select {
 	case <-pullCmd.Done():
 		cancel()
-		tp.Infof("test async: %v", result)
+		tp.Infof("test async1: %v", result)
 	case <-ctx.Done():
-		tp.Warnf("test async: client context timeout: %v", ctx.Err())
+		tp.Warnf("test async1: client context timeout: %v", ctx.Err())
 	}
 
-	time.Sleep(time.Second * 5)
-	rerr := sess.Pull("/test/ok", "test4", &result).Rerror()
-	tp.Warnf("test sync 3: disconnect due to server session timeout: %v", rerr.ToError())
+	time.Sleep(time.Second * 6)
+	result = ""
+	rerr = sess.Pull("/test/ok", "test4", &result).Rerror()
+	tp.Warnf("test sync3: disconnect due to server session timeout: %v", rerr.ToError())
+
+	sess, err = cli.Dial(":9090")
+	if err != nil {
+		tp.Fatalf("%v", err)
+	}
+	sess.AsyncPull(
+		"/test/break",
+		nil,
+		nil,
+		make(chan tp.PullCmd, 1),
+	)
 }
