d6a40c7a286af2f214d6593592cb4ddc17fe7f48
henrylee2cn <henrylee2cn@gmail.com>
2019-02-16 05:40:07 +0800

    chore(websocket): Improve ease of use
    Change-Id: I39e0c40a45ebfd7481df0b8ac8586664ee4bca47

--- mixer/websocket/README.md
+++ mixer/websocket/README.md
@@ -12,6 +12,7 @@ Websocket is an extension package that makes the Teleport framework compatible w
 package websocket_test
 
 import (
+	"crypto/tls"
 	"net/http"
 	"testing"
 	"time"
@@ -19,7 +20,6 @@ import (
 	tp "github.com/henrylee2cn/teleport"
 	ws "github.com/henrylee2cn/teleport/mixer/websocket"
 	"github.com/henrylee2cn/teleport/mixer/websocket/jsonSubProto"
-	"github.com/henrylee2cn/teleport/mixer/websocket/pbSubProto"
 )
 
 type Arg struct {
@@ -33,15 +33,15 @@ func (p *P) Divide(arg *Arg) (int, *tp.Rerror) {
 	return arg.A / arg.B, nil
 }
 
-func TestJSONSubWebsocket(t *testing.T) {
-	srv := tp.NewPeer(tp.PeerConfig{})
-	http.Handle("/ws", ws.NewJSONServeHandler(srv, nil))
-	go http.ListenAndServe("0.0.0.0:9090", nil)
+func TestJSONWebsocket(t *testing.T) {
+	srv := ws.NewServer("/ws", tp.PeerConfig{ListenPort: 9090})
 	srv.RouteCall(new(P))
+	go srv.ListenAndServe()
+
 	time.Sleep(time.Second * 1)
 
-	cli := tp.NewPeer(tp.PeerConfig{}, ws.NewDialPlugin("/ws"))
-	sess, err := cli.Dial("127.0.0.1:9090", jsonSubProto.NewJSONSubProtoFunc())
+	cli := ws.NewClient(":9090", "/ws", tp.PeerConfig{})
+	sess, err := cli.Dial()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -58,15 +58,42 @@ func TestJSONSubWebsocket(t *testing.T) {
 	time.Sleep(time.Second)
 }
 
-func TestPbSubWebsocket(t *testing.T) {
+func TestPbWebsocketTLS(t *testing.T) {
+	srv := ws.NewServer("/ws", tp.PeerConfig{ListenPort: 9091})
+	srv.RouteCall(new(P))
+	srv.SetTLSConfig(tp.GenerateTLSConfigForServer())
+	go srv.ListenAndServeProtobuf()
+
+	time.Sleep(time.Second * 1)
+
+	cli := ws.NewClient(":9091", "/ws", tp.PeerConfig{})
+	cli.SetTLSConfig(&tls.Config{InsecureSkipVerify: true})
+	sess, err := cli.DialProtobuf()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var result int
+	rerr := sess.Call("/p/divide", &Arg{
+		A: 10,
+		B: 2,
+	}, &result,
+	).Rerror()
+	if rerr != nil {
+		t.Fatal(rerr)
+	}
+	t.Logf("10/2=%d", result)
+	time.Sleep(time.Second)
+}
+
+func TestCustomizedWebsocket(t *testing.T) {
 	srv := tp.NewPeer(tp.PeerConfig{})
-	http.Handle("/ws", ws.NewPbServeHandler(srv, nil))
-	go http.ListenAndServe("0.0.0.0:9091", nil)
+	http.Handle("/ws", ws.NewJSONServeHandler(srv, nil))
+	go http.ListenAndServe(":9092", nil)
 	srv.RouteCall(new(P))
 	time.Sleep(time.Second * 1)
 
 	cli := tp.NewPeer(tp.PeerConfig{}, ws.NewDialPlugin("/ws"))
-	sess, err := cli.Dial("127.0.0.1:9091", pbSubProto.NewPbSubProtoFunc())
+	sess, err := cli.Dial(":9092", jsonSubProto.NewJSONSubProtoFunc())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -87,11 +114,12 @@ func TestPbSubWebsocket(t *testing.T) {
 test command:
 
 ```sh
-go test -v -run=TestJSONSubWebsocket
-go test -v -run=TestPbSubWebsocket
+go test -v -run=TestJSONWebsocket
+go test -v -run=TestPbWebsocketTLS
+go test -v -run=TestCustomizedWebsocket
 ```
 
-Among them, TestJSONSubWebsocket's request body is:
+Among them, TestJSONWebsocket's request body is:
 
 ```json
 {
@@ -105,7 +133,7 @@ Among them, TestJSONSubWebsocket's request body is:
 }
 ```
 
-TestJSONSubWebsocket's response body is:
+TestJSONWebsocket's response body is:
 
 ```json
 {
--- mixer/websocket/server.go
+++ mixer/websocket/server.go
@@ -15,6 +15,9 @@
 package websocket
 
 import (
+	"context"
+	"errors"
+	"net"
 	"net/http"
 	"net/url"
 
@@ -23,6 +26,105 @@ import (
 	"github.com/henrylee2cn/teleport/mixer/websocket/pbSubProto"
 	ws "github.com/henrylee2cn/teleport/mixer/websocket/websocket"
 )
+
+// Server a websocket server
+type Server struct {
+	tp.Peer
+	cfg       tp.PeerConfig
+	serveMux  *http.ServeMux
+	server    *http.Server
+	pattern   string
+	lis       net.Listener
+	handshake func(*ws.Config, *http.Request) error
+}
+
+// NewServer creates a websocket server.
+func NewServer(pattern string, cfg tp.PeerConfig, globalLeftPlugin ...tp.Plugin) *Server {
+	p := tp.NewPeer(cfg, globalLeftPlugin...)
+	serveMux := http.NewServeMux()
+	return &Server{
+		Peer:     p,
+		cfg:      cfg,
+		serveMux: serveMux,
+		pattern:  pattern,
+		server:   &http.Server{Addr: cfg.ListenerAddr(), Handler: serveMux},
+	}
+}
+
+// ListenAndServeJSON listen and serve with the JSON protocol.
+func (srv *Server) ListenAndServeJSON() error {
+	return srv.ListenAndServe(jsonSubProto.NewJSONSubProtoFunc())
+}
+
+// ListenAndServeProtobuf listen and serve with the Protobuf protocol.
+func (srv *Server) ListenAndServeProtobuf() error {
+	return srv.ListenAndServe(pbSubProto.NewPbSubProtoFunc())
+}
+
+// ListenAndServe listens on the TCP network address addr and then calls
+// Serve with handler to handle requests on incoming connections.
+// Accepted connections are configured to enable TCP keep-alives.
+//
+// The handler is typically nil, in which case the DefaultServeMux is used.
+//
+// ListenAndServe always returns a non-nil error.
+//
+// If protoFunc is empty, JSON is used by default.
+func (srv *Server) ListenAndServe(protoFunc ...tp.ProtoFunc) (err error) {
+	network := srv.cfg.Network
+	switch network {
+	default:
+		return errors.New("Invalid network config, refer to the following: tcp, tcp4, tcp6")
+	case "tcp", "tcp4", "tcp6":
+	}
+	srv.Handle(srv.pattern, NewServeHandler(srv.Peer, srv.handshake, protoFunc...))
+	addr := srv.cfg.ListenerAddr()
+	if addr == "" {
+		if srv.Peer.TLSConfig() != nil {
+			addr = ":https"
+		} else {
+			addr = ":http"
+		}
+	}
+	srv.lis, err = tp.NewInheritedListener(network, addr, srv.Peer.TLSConfig())
+	if err != nil {
+		return
+	}
+	addr = srv.lis.Addr().String()
+	tp.Printf("listen and serve (network:%s, addr:%s)", network, addr)
+	for _, v := range srv.Peer.PluginContainer().GetAll() {
+		if p, ok := v.(tp.PostListenPlugin); ok {
+			p.PostListen(srv.lis.Addr())
+		}
+	}
+	return srv.server.Serve(srv.lis)
+}
+
+// Close closes the server.
+func (srv *Server) Close() error {
+	err := srv.server.Shutdown(context.Background())
+	if err != nil {
+		srv.Peer.Close()
+		return err
+	}
+	return srv.Peer.Close()
+}
+
+// SetHandshake sets customized handshake function.
+func (srv *Server) SetHandshake(handshake func(*ws.Config, *http.Request) error) {
+	srv.handshake = handshake
+}
+
+// Handle registers the handler for the given pattern.
+// If a handler already exists for pattern, Handle panics.
+func (srv *Server) Handle(pattern string, handler http.Handler) {
+	srv.serveMux.Handle(pattern, handler)
+}
+
+// HandleFunc registers the handler function for the given pattern.
+func (srv *Server) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	srv.serveMux.HandleFunc(pattern, handler)
+}
 
 // NewJSONServeHandler creates a websocket json handler.
 func NewJSONServeHandler(peer tp.Peer, handshake func(*ws.Config, *http.Request) error) http.Handler {
