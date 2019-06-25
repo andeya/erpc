0d41ef6759a4b2647f009b2f4d658029ef717495
henrylee2cn <henrylee2cn@gmail.com>
2019-06-25 22:10:47 +0800

    style(proto): Update thrift proto test
    Change-Id: I03ac5c1954a04261b212804272c89ae94b2eb114

--- proto/thriftproto/test.thrift
+++ proto/thriftproto/test.thrift
@@ -1,9 +1,9 @@
-namespace go thriftproto
-namespace perl thriftproto
-namespace py thriftproto
-namespace cpp thriftproto
-namespace rb thriftproto
-namespace java thriftproto
+namespace go thriftproto_test
+namespace perl thriftproto_test
+namespace py thriftproto_test
+namespace cpp thriftproto_test
+namespace rb thriftproto_test
+namespace java thriftproto_test
 
 struct test {
     1: string  author,
